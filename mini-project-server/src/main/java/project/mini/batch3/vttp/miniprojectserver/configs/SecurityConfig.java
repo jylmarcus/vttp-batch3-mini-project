30be46eef97d2d4822db9ff652b60da1d3e9d663
Hamukaze <maplecow@gmail.com>
2023-09-14 12:06:28 +0800

    Added http request

--- mini-project-server/src/main/java/project/mini/batch3/vttp/miniprojectserver/configs/SecurityConfig.java
+++ mini-project-server/src/main/java/project/mini/batch3/vttp/miniprojectserver/configs/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests((requests) -> requests
+                    .requestMatchers(HttpMethod.GET, "/").permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                     .anyRequest().authenticated());
         return http.build();
